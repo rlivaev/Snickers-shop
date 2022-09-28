9087ca8c942a64510fc9c0ad2cf1682272e407db
Rustam <ruslivaev05@gmail.com>
2022-09-28 23:59:44 +0600

    add gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg/>
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg/>
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <svg/>
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
     </div>
   );
 }
-
 export default App;
